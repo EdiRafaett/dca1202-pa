03240b35bf868ddcf2ca3b659e72c0936540f054
EdiRafaett <edivelton545@hotmail.com>
2024-04-01 23:56:32 -0300

    alterações

--- listac/questao15.c
+++ listac/questao15.c
@@ -1,32 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int *x, i, n, j, temp;
-    //Quantidade de valores a serem ordenados:
-    printf("Digite quantidade de valores que deseja ordenar: ");
-    scanf("%d", &n);
-    x = malloc(n * sizeof(int));
-    // Recebendo os valores:
-    for(int i = 0; i < n; i ++){
-        printf("Digite o valor %d\n", (i+1)); 
-        scanf("%d", &x[i]);
-    }
-    //ordenação:
-        for (i = 0; i < n - 1; i++) {
+// Função para trocar dois elementos
+void swap(float *a, float *b) {
+    float temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Função para ordenar um array usando Bubble Sort
+void bubbleSort(float arr[], int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
-            if (x[j] > x[j + 1]) {
-            // Troca os elementos se estiverem fora de ordem
-                temp = x[j];
-                x[j] = x[j + 1];
-                x[j + 1] = temp;
+            if (arr[j] > arr[j + 1]) {
+                // Chama a função swap para trocar os elementos
+                swap(&arr[j], &arr[j + 1]);
             }
         }
     }
-    for(i = 0; i < n; i++) {
-        printf("%d, ", x[i]);
-        }
-        printf("\n");
-        return 0;
-        free(x);
+}
+
+int main() {
+    float *x;
+    int i, n;
+
+    // Quantidade de valores a serem ordenados
+    printf("Digite a quantidade de valores que deseja ordenar: ");
+    scanf("%d", &n);
+
+    // Verifica se a alocação de memória foi bem-sucedida
+    x = malloc(n * sizeof(float));
+    if (x == NULL) {
+        printf("Erro de alocação de memória");
+        return 1;
+    }
+
+    // Recebendo os valores
+    for (i = 0; i < n; i++) {
+        printf("Digite o valor %d: ", (i + 1));
+        scanf("%f", &x[i]);
+    }
+
+    // Chama a função bubbleSort para ordenar o array
+    bubbleSort(x, n);
+
+    // Imprime os valores ordenados
+    printf("Valores ordenados: ");
+    for (i = 0; i < n; i++) {
+        printf("%f ", x[i]);
+    }
+    printf("\n");
+
+    // Libera a memória alocada
+    free(x);
+
+    return 0;
 }
--- listac/questao24.c
+++ listac/questao24.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
-#include <math.h>
-//função de saida :
-unsigned long send(unsigned long estado){
-  printf("Estado enviado: %lu\n", estado);
+void send (unsigned long estado){
+    printf("número de saída: %lu\n", estado);
 }
-unsigned long codificadora(unsigned char m[8][8]) {
-    int i, j, cont = 63; 
+
+// Função para converter uma matriz 8x8 de 0s e 1s em um unsigned long
+unsigned long matriz_para_ulong(unsigned char matriz[8][8]) {
     unsigned long estado = 0;
-  //varrendo a matriz somando as potências de dois:
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            if (m[i][j] == 1) {
-                estado = estado + (unsigned long)pow(2, cont); 
-            }
-            cont--;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            // Desloca o resultado para a esquerda (multiplicação por 2) e adiciona o valor da célula.
+            //em outras palavras: << "empurra" os bits uma posição a esquerda liberando um espaço e o | coloca o bit seguinte nesse espaço"
+            estado = (estado << 1) | matriz[i][j];
         }
     }
-  send(estado);
-  
+    //enviando resultado para a função de saída:
+    send(estado);
+    return estado;
 }
 
 int main() {
-  //matriz modificável:
-    unsigned char m[8][8] = {
+    // Exemplo de matriz 8x8 com 0s e 1s
+    unsigned char matriz[8][8] = {
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1},
         {1, 1, 1, 1, 1, 1, 1, 1}
     };
-  //enviando matriz para a função codificadora:
-  codificadora(m);
+
+    // Enviando a matriz para a função:
+    matriz_para_ulong(matriz);
+
     return 0;
 }
-
-
-
-/*OBS para o aprendizado de funções: Quando dentro da main ou outra função, informo o nome de uma função
- e passo um parâmetro ou mais, o valor desses parâmetros passados chgarão nas respectivas posições nas variáveis declaradas no escopo da função*/
